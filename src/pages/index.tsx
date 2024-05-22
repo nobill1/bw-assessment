9d108b172519efdcfdf19d80049f1c260a771ec8
Bill Nguimeya <bnguimeya007@gmail.com>
2024-05-22 14:15:46 +0100

    project setup, design placement, spin animation on click

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,117 +1,203 @@
+import { useState } from 'react';
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
+const myFont = localFont({ src: '../../public/nKKF-GM_FYFRJvXzVXaAPe97P1KHynJFP716qNl5yoKZiA.woff2' });
 
 export default function Home() {
+  const [currentImage, setCurrentImage] = useState('/0.webp');
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const images = ['/blur1.webp', '/blur2.webp', '/blur3.webp'];
+  const finalImages = ['/1.webp', '/2.png', '/3.png'];
+
+  const handleClick = () => {
+    if (isAnimating) return; // Prevent multiple clicks while animating
+
+    setIsAnimating(true);
+
+    let counter = 0;
+    const interval = setInterval(() => {
+      setCurrentImage(images[counter % images.length]);
+      counter++;
+    }, 100);
+
+    setTimeout(() => {
+      clearInterval(interval);
+      setCurrentImage(finalImages[Math.floor(Math.random() * finalImages.length)]);
+      setIsAnimating(false);
+    }, 1500); // Run the animation for 1 second
+  };
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`relative flex min-h-screen flex-col items-center justify-between md:justify-center ${myFont.className} bg-star-pattern bg-cover bg-center`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+      <div className="container mx-auto flex flex-col justify-center items-center gap-8 pt-20 md:pt-0 px-3 z-20 md:h-auto">
+        <div className="relative flex bg-game-pattern bg-cover border-4 border-[#fe5071] rounded-3xl md:mx-auto">
+          <div className="flex justify-evenly items-center absolute top-0 right-0 left-0 bottom-0">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
+              src="/game-seperator.png"
+              alt="game-seperator"
+              className="h-full"
+              width={4}
+              height={353}
               priority
             />
-          </a>
+            <Image
+              src="/game-seperator.png"
+              alt="game-seperator"
+              className="h-full"
+              width={4}
+              height={353}
+              priority
+            />
+          </div>
+          <div className="flex justify-center items-center absolute top-0 right-0 left-0 -translate-y-1/2 px-6">
+            <Image
+              src="/vegasplus.webp"
+              alt="vegasplus"
+              width={398}
+              height={194}
+              priority
+            />
+          </div>
+          <div className={`flex px-6 w-full h-auto ${isAnimating ? 'slide-up' : ''}`}>
+            <Image
+              src={currentImage}
+              alt="0"
+              style={{ objectFit: 'cover' }}
+              // className="w-full h-auto"
+              width={511}
+              height={347}
+              priority
+            />
+          </div>
         </div>
+
+        <button onClick={handleClick} className="self-center bg-[#fe5071] py-2 px-14 rounded-xl text-3xl lg:text-5xl animate-shrinkGrow">SPIN NOW</button>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className="flex relative translate-y-4 2xl:translate-y-0 justify-center md:absolute md:top-0 md:left-0 md:right-0 md:bottom-0 md:justify-between">
+        <div className="flex justify-stretch opacity-40 md:hidden">
+          <div className="flex self-end translate-y-16 opacity-30">
+            <Image
+              src="/shield.webp"
+              alt="shield"
+              width={176}
+              height={270}
+              priority
+            />
+          </div>
+          <div className="flex self-end -translate-y-10">
+            <Image
+              src="/dragon.webp"
+              alt="dragon"
+              width={222}
+              height={286}
+              priority
+            />
+          </div>
+          <div className="flex self-end opacity-30">
+            <Image
+              src="/star.webp"
+              alt="star"
+              width={213}
+              height={213}
+              priority
+            />
+          </div>
+        </div>
+        <div className="flex justify-stretch absolute top-0 bottom-0 left-0 right-0 z-10 md:hidden">
+          <div className="flex justify-center items-center translate-x-16">
+            <Image
+              src="/zeus.webp"
+              alt="zeus"
+              width={258}
+              height={508}
+              priority
+            />
+          </div>
+          <div className="flex self-end z-10">
+            <Image
+              src="/spinata.webp"
+              alt="spinata"
+              width={254}
+              height={212}
+              priority
+            />
+          </div>
+          <div className="flex justify-center items-center -translate-x-16 -translate-y-5">
+            <Image
+              src="/riche.webp"
+              alt="riche"
+              width={268}
+              height={555}
+              priority
+            />
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {/* large screen */}
+        <div className="container mx-auto flex justify-between xl:justify-evenly w-full self-stretch">
+          <div className="md:flex flex-col justify-center items-center xl:-translate-x-20 hidden">
+            <div className="flex self-end justify-end translate-y-[330px] -translate-x-28 opacity-40 ">
+              <Image
+                src="/shield.webp"
+                alt="shield"
+                width={176}
+                height={270}
+                priority
+              />
+            </div>
+            <div className="flex justify-center items-center z-10">
+              <Image
+                src="/zeus.webp"
+                alt="zeus"
+                width={258}
+                height={508}
+                priority
+              />
+            </div>
+            <div className="flex self-end -translate-y-44 -translate-x-[26px] z-20">
+              <Image
+                src="/dragon.webp"
+                alt="dragon"
+                width={222}
+                height={286}
+                priority
+              />
+            </div>
+          </div>
+          <div className="md:flex flex-col justify-center items-center xl:translate-x-20 -translate-y-9 hidden">
+            <div className="flex self-end justify-end translate-y-[450px] opacity-40">
+              <Image
+                src="/star.webp"
+                alt="star"
+                width={213}
+                height={213}
+                priority
+              />
+            </div>
+            <div className="flex justify-center items-center z-10">
+              <Image
+                src="/riche.webp"
+                alt="riche"
+                width={268}
+                height={555}
+                priority
+              />
+            </div>
+            <div className="flex self-end -translate-y-28 -translate-x-2 z-10">
+              <Image
+                src="/spinata.webp"
+                alt="spinata"
+                width={254}
+                height={212}
+                priority
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,20 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
+        'star-pattern': "url('/background.webp')",
+        'game-pattern': "url('/game-bg.webp')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        shrinkGrow: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(0.9)' },
+        },
+      },
+      animation: {
+        shrinkGrow: 'shrinkGrow 0.7s infinite',
       },
     },
   },
